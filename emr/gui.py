1fd64bb9ed09ab34d0c39f9694e14dc7a25b3d2a
Lily <lilywanshu@qq.com>
2022-02-04 18:56:09 +0800

    the final program for emr

--- emr/gui.py
+++ emr/gui.py
@@ -8,10 +8,12 @@ import PySimpleGUI as sg
 from emr import SCHEDULE
 import emr.main
 import emr
+import emr.db
 
 sg.theme('DarkAmber')    # Keep things interesting for your users
 font_title = ('Arial', 40)
 font_body = ('Arial', 20)
+default_text_color = '#fdcb52'
 
 def build_task1():
     rows = [
@@ -24,7 +26,7 @@ def build_task1():
         sold = emr.AVAILABLE_INNITIAL - available
         rows.append([
             sg.Text(f'UP {up[i]}    DOWN {down[i]}', font=font_body),
-            sg.Text(f'Available {available}    Sold {sold}', font=font_body),
+            sg.Text(f'Available {available}    Sold {sold}', font=font_body, key=f'-task_1_available{i}-'),
             sg.Button('Purchase', font=font_body, key=f'-purchase{i}-')
         ])
     rows.append([sg.Button('END', font=font_body)])
@@ -58,7 +60,7 @@ def build_task3():
         sold = emr.AVAILABLE_INNITIAL - available
         rows.append([
             sg.Text(f'UP {up[i]}    DOWN {down[i]}', font=font_body),
-            sg.Text(f'Sold {sold}   ', font=font_body), sg.Text('price ', key='-everyprice-', font=font_body),
+            sg.Text(f'Sold {sold}   ', font=font_body, key=f'-everysold{i}-'), sg.Text('price ', key=f'-everyprice{i}-', font=font_body),
             sg.Text('CLOSED', font=font_body, text_color='grey')
         ])
     rows.append([sg.Text('Total price ', font=font_body, key='-totalprice-'), sg.Text('Total tickets ', font=font_body, key='-totaltickets-')])
@@ -66,9 +68,18 @@ def build_task3():
     return rows
 
 def change_task(task_index):
-    for i in (1, 2, 3):
+    for i in (0, 1, 2, 3):
         window[f'-task{i}-'].update(visible=False)
     window[f'-task{task_index}-'].update(visible=True)
+    if task_index == 1:
+        window['-tickets-'].update(value=1)
+
+def update_task1():
+    for i in range(len(emr.data)):
+        available = emr.data[i]['ticket']
+        sold = emr.AVAILABLE_INNITIAL - available
+        window[f'-task_1_available{i}-'].update(value=f'Available {available}    Sold {sold}' if available > 0 else 'Closed', text_color=default_text_color if available > 0 else 'grey')
+        window[f'-purchase{i}-'].update(visible=available > 0)
 
 def update_task2(train_index, ticket):
     window['-caution-'].update(value='')
@@ -87,9 +98,38 @@ def update_task2(train_index, ticket):
         window['-caution-'].update(value=str(e))
     return 0
 
+def update_task3():
+    index_sold = []
+    for i in range(len(emr.data)):
+        available = emr.data[i]['ticket']
+        price = emr.data[i]['price']
+        sold = emr.AVAILABLE_INNITIAL - available
+        index_sold.append((i, sold))
+        window[f'-everysold{i}-'].update(value=f'Sold {sold}    ', text_color=default_text_color)
+        window[f'-everyprice{i}-'].update(value=f'price {price}', text_color=default_text_color)
+
+    sold_list = [item[1] for item in index_sold]
+    sold_list.sort()
+    max_sold = sold_list[-1]
+    maxi = []
+    for item in index_sold:
+        if max_sold == item[1]:
+            maxi.append(item[0])
+    
+    if len(maxi) > 0:
+        print(maxi)
+        for i in maxi:
+            window[f'-everysold{i}-'].update(text_color='red')
+            window[f'-everyprice{i}-'].update(text_color='red')
+
+    total_tickets, total_price = emr.main.get_total()
+    window['-totalprice-'].update(value=f'Total price {total_price}    ')
+    window['-totaltickets-'].update(value=f'Total tickets {total_tickets}')
+
 layout = [
     [
-        sg.Column(build_task1(), key='-task1-', visible=True),
+        sg.Column([[sg.Button('Enter', font=font_title)]], key='-task0-', visible=True),
+        sg.Column(build_task1(), key='-task1-', visible=False),
         sg.Column(build_task2(), key='-task2-', visible=False),
         sg.Column(build_task3(), key='-task3-', visible=False),
     ]
@@ -100,15 +140,22 @@ window = sg.Window('Electric mountain railway(by Lily)', layout, size=(800, 600)
 
 def main_loop():
     train_index = 0 
-    while True:                             # The Event Loop
+    while True:                  
         event, values = window.read() 
         print(event, values)
         if event == sg.WIN_CLOSED:
             break      
-        if event == 'END':
+        if event == 'Enter':
+            update_task1()
+            change_task(1)
+        elif event == 'END':
+            update_task3()
             change_task(3)
         elif event == '-start-':
+            emr.db.del_db()
+            emr.data = emr.db.load_db()
             change_task(1)
+            update_task1()
         elif event.startswith('-purchase'):
             change_task(2)
             train_index = int(event[-2])
@@ -126,4 +173,5 @@ def main_loop():
             if ticket > 0:
                 emr.main.buy_ticket(train_index, ticket)
                 change_task(1)
+                update_task1()
     window.close()
